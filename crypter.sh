66a8950a49bfe75f08de6f467d56ea96e431002b
Frank Bischof <info@meer-web.nl>
2024-02-21 11:51:50 +0100

    Added working folder

--- crypter.sh
+++ crypter.sh
@@ -16,12 +16,12 @@ else
 fi
 
 # Check if keys are set
-if [ ! -f ~/.crypter/keys ]
+if [ ! -f ~/.crypter/.keys ]
 then
 	mkdir -p ~/.crypter
-	echo "KEYS=\"-des-ede3-cbc -pbkdf2\"" > keys
+	echo "KEYS=\"-des-ede3-cbc -pbkdf2\"" > ~/.crypter/.keys
 fi
-source ~/.crypter/keys
+source ~/.crypter/.keys
 
 # Check method
 case "$1" in 
